1ca41cf9b38128e82ec152f1bfdf927e24f06cf8
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-06 09:32:51 -0500

    Add: middleware logger

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -2,6 +2,7 @@ import { type StateCreator, create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 // import { customSessionStorages } from "../storages/session.storage";
 import { firebaseStorage } from "../storages/firebase.storage";
+// import { logger } from "../middlewares/logger.middleware";
 
 interface PersonState {
   firstName: string;
